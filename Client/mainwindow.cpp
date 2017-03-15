519f8f4bd357fe241487041fc2d3fd5e060e398a
Terry <rkdxogns1234@daum.net>
2017-03-15 03:22:58 -0700

    Added playing wav files with a fixed header format and no audio buffer

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -6,9 +6,144 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    isPlaying = false;
+    isPaused = false;
+
+    // Create model
+    available_song_model = new QStringListModel(this);
+    playlist_model = new QStringListModel(this);
+
+    // Glue model and view together
+    ui->listView_availSongs->setModel(available_song_model);
+    ui->listView_playlist->setModel(playlist_model);
+
+    findAvailableSongs();
+
+    initAudioOuput();
+
+}
+void MainWindow::findAvailableSongs(){
+    QStringList nameFilter("*.wav");
+    QDir directory("../assets/musics");
+    QStringList wavFilesList = directory.entryList(nameFilter);
+
+    // Populate the model
+    available_song_model->setStringList(wavFilesList);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_button_addSong_clicked()
+{
+    QModelIndex index = ui->listView_availSongs->currentIndex();
+    if(index.row() < 0)
+        return;
+
+    QString itemText = index.data(Qt::DisplayRole).toString();
+
+
+    playlist_model->insertRow(playlist_model->rowCount());
+    QModelIndex row = playlist_model->index(playlist_model->rowCount()-1);
+    playlist_model->setData(row, itemText);
+}
+
+void MainWindow::on_button_play_clicked()
+{
+    playNextSong();
+}
+
+void MainWindow::initAudioOuput(){
+    QAudioFormat format;
+    // Set up the format, eg.
+    format.setSampleRate(44100);
+    format.setChannelCount(2);
+    format.setSampleSize(16);
+    format.setCodec("audio/pcm");
+    format.setByteOrder(QAudioFormat::LittleEndian);
+    format.setSampleType(QAudioFormat::UnSignedInt);
+
+    QAudioDeviceInfo info(QAudioDeviceInfo::defaultOutputDevice());
+    if (!info.isFormatSupported(format)) {
+        qWarning() << "Raw audio format not supported by backend, cannot play audio.";
+        return;
+    }
+
+    audio = new QAudioOutput(format, this);
+    connect(audio, SIGNAL(stateChanged(QAudio::State)), this, SLOT(handleStateChanged(QAudio::State)));
+}
+
+void MainWindow::playNextSong(){
+    if(!isPlaying){
+        if(playlist_model->stringList().length() <= 0)
+            return;
+        isPlaying = true;
+
+        if(audio && audio->state() == QAudio::SuspendedState){
+            audio->resume();
+            return;
+        }
+
+        QFile sourceFile;
+        sourceFile.setFileName("../assets/musics/" + playlist_model->stringList().at(0));
+        sourceFile.open(QIODevice::ReadOnly);
+
+
+        audio->start(&sourceFile);
+        QEventLoop loop;
+        do {
+            loop.exec();
+        } while(audio->state() == QAudio::ActiveState);
+    }else{
+        audio->suspend();
+        isPlaying = false;
+    }
+}
+
+void MainWindow::handleStateChanged(QAudio::State newState)
+{
+    switch (newState) {
+        case QAudio::IdleState:
+            qDebug() << "IdleState";
+            // Finished playing (no more data)
+            if(isPlaying){
+                isPlaying = false;
+                playlist_model->removeRow(0);
+                audio->stop();
+                sourceFile.close();
+                playNextSong();
+            }
+            break;
+
+        case QAudio::StoppedState:
+            qDebug() << "StoppedState";
+            // Stopped for other reasons
+            if (audio->error() != QAudio::NoError) {
+                // Error handling
+            }
+            break;
+        case QAudio::SuspendedState:
+            qDebug() << "SuspendedState";
+            break;
+        case QAudio::ActiveState:
+            qDebug() << "ActiveState";
+            break;
+        default:
+            // ... other cases as appropriate
+            break;
+    }
+}
+
+void MainWindow::on_button_skip_clicked()
+{
+    if(isPlaying){
+        isPlaying = false;
+        playlist_model->removeRow(0);
+        audio->stop();
+        sourceFile.close();
+        playNextSong();
+    }
 }
--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDir>
+#include <QStringListModel>
+#include <QDebug>
+#include <QAudioOutput>
+
 
 namespace Ui {
 class MainWindow;
@@ -15,15 +20,29 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void findAvailableSongs();
 private slots:
-    void on_pushButton_6_clicked();
 
-    void on_pushButton_8_clicked();
+    void on_button_addSong_clicked();
 
-    void on_ProgressSlider_sliderPressed();
+    void on_button_play_clicked();
+
+    void handleStateChanged(QAudio::State newState);
+    void on_button_skip_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QStringListModel *available_song_model;
+    QStringListModel *playlist_model;
+    QFile sourceFile;   // class member.
+    QAudioOutput* audio; // class member.
+    bool isPlaying;
+    bool isPaused;
+
+    void playNextSong();
+    void initAudioOuput();
+
+
 };
 
 #endif // MAINWINDOW_H
